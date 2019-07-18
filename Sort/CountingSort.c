6bb6d294a76c212cef7f8f89c635ac4a2b518315
NINGNINGGAO <707288044@qq.com>
2019-07-18 18:03:27 +0800

    modify CountingSort 7/18 18/03

--- Sort/CountingSort.c
+++ Sort/CountingSort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void CountingSort(int* nums, int numsSize, int** numsSort) {
+void CountingSort(int* nums, int numsSize) {
           int  numsmin = nums[0], numsmax = nums[0];
           for (int i = 1; i < numsSize; i++) {
                     if (nums[i] > numsmax) {
@@ -19,19 +19,21 @@ void CountingSort(int* nums, int numsSize, int** numsSort) {
           for (int k = 1; k < numslength; k++) {
                     C[k] = C[k] + C[k - 1];
           }
-          *numsSort = (int*)malloc(sizeof(int) * numsSize);
+          int* numsSort = (int*)malloc(sizeof(int) * numsSize);
           for (int h = numsSize - 1; h >= 0; h--) {
-                    (*numsSort)[C[nums[h] - numsmin] - 1] = nums[h];
+                    numsSort[C[nums[h] - numsmin] - 1] = nums[h];
                     C[nums[h] - numsmin]--;
+          }
+          for (int i = 0; i < numsSize; i++) {
+                    nums[i] = numsSort[i];
           }
 }
 
 main() {
-          int* numsSort;
           int nums[] = {7,8,9,4,5,6,2,3,17,18,19,15,16,12,13,50};
           int numsSize = sizeof(nums) / sizeof(int);
-          CountingSort(nums, numsSize, &numsSort);
+          CountingSort(nums, numsSize);
           for (int i = 0; i < numsSize; i++) {
-                    printf("%d\n", numsSort[i]);
+                    printf("%d\n", nums[i]);
           }
 }
