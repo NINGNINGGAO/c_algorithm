45b6bda7392bfe22219948bd7150d9819f2146e7
NINGNINGGAO <707288044@qq.com>
2019-07-19 19:59:10 +0800

    add BucketSort.c 7/19 19/59

--- Sort/BucketSort.c
+++ Sort/BucketSort.c
@@ -1,50 +1,46 @@
 #include <stdio.h>
 
-typedef struct {
-          int val;
-          Node* prev;
-          Node* next;
+typedef struct LINK {
+    float val;
+    struct LINK* prev;
+    struct LINK* next;
 } Node;
 
-void BucketSort(int* nums, int numsSize) {
-          Node* B = (Node*)malloc(sizeof(Node) * numsSize);
+void BucketSort(float* nums, int numsSize) {
+          Node B[numsSize], temp[numsSize];
           for (int i = 0; i < numsSize; i++) {
-                    B[i] -> prev = NULL;
-                    B[i] -> next = NULL;
+              B[i] = (Node){0, NULL, NULL};
+              temp[i] = (Node){nums[i], NULL, NULL};
           }
+          Node* mid;
           for (int i = 0; i < numsSize; i++) {
-                    Node* temp;
-                    temp -> val = nums[i];
-                    temp -> prev = B[(int)(numsSize * nums[i])]; 
-                    temp -> next = B[(int)(numsSize * nums[i])] -> next;
-                    B[(int)(numsSize * nums[i])] -> next = temp;
-          }
-          for (int i = 0; i < numsSize; i++) {
-                    temp = B[i] -> next;
-                    while (temp -> next != NULL) {
-                                if ((temp ->next -> val) < (temp -> val)) {
-                                            temp -> prev ->next = temp -> next; //delete temp
-                                            temp -> next - prev = temp -> prev;  // delete temp
-                                            temp -> prev = temp -> next; // insert temp
-                                            temp -> next = temp -> next -> next; //insert temp
-                                }
-                    }
-
+              mid = &B[(int)(numsSize * nums[i])];
+              while (mid->next != NULL && temp[i].val >= mid->next->val) {
+                  mid = mid->next;
+              } 
+              temp[i].prev = mid;
+              temp[i].next = mid->next;
+              if (mid->next != NULL) {
+                  mid->next->prev = &temp[i];
+              }
+              mid->next = &temp[i];
           }
           int j = 0;
+          Node* h;
           for (int i = 0; i < numsSize; i++) {
-              temp = B[i] -> next;
-              while (temp != NULL) {
-                  nums[j++] = temp -> val;
+              h = B[i].next;
+              while (h != NULL) {
+                  nums[j++] = h->val;
+                  h = h->next;
               }
           }
 }
 
 main() {
-    int nums[] = {7,8,9,4,5,6,1,2,3};
+    float nums[] = {0.78,0.17,0.39,0.26,0.72,0.94,0.21,0.12,0.23,0.68,0.01,0.05,0.56,0.60,0.88,0.66,0.99};
     int numsSize = sizeof(nums) / sizeof(int);
-    BuckerSort(nums, numsSize);
+    BucketSort(nums, numsSize);
     for (int i = 0; i < numsSize; i++) {
-        printf("%d\n", nums[i]);
+        printf("%f\n", nums[i]);
     }
 }
