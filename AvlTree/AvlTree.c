5a55a5777f3e04dd3776e4e98c19eb07e753177a
NINGNINGGAO <707288044@qq.com>
2019-07-28 12:33:30 +0800

    delete AvlTree.c/mian 7/28 12/33

--- AvlTree/AvlTree.c
+++ AvlTree/AvlTree.c
@@ -266,27 +266,3 @@ AvlTree Delete(ElementType X, AvlTree T) {
           }
           return T;
 }
-
-main() {
-          AvlTree T;
-          Position P;
-          T = malloc(sizeof(struct AvlNode));
-          T->Element = 7;
-          T->Parents = NULL;
-          T->Left = NULL;
-          T->Right = NULL;
-          for (int i = 1; i < 7; i++) {
-                    if (i == 10) {
-                              continue;
-                    }
-                    T = Insert(i, T);
-          }
-          for (int i = 1; i < 8; i++) {
-                    T = Delete(i, T);
-          }
-          printf("%d\n", Retrieve(FindMax(T)));
-          printf("%d\n", Retrieve(FindMin(T)));
-          T = MakeEmpty(T);
-          printf("%d\n", Retrieve(FindMax(T)));
-          printf("%d\n", Retrieve(FindMin(T)));
-}
