003bcb73609b4c9cbdc6a8006b61ba31ca53f36b
NINGNINGGAO <707288044@qq.com>
2019-08-02 16:39:25 +0800

    add KMP 8/2 16/39

---
+++ KMP/KMP.c
@@ -0,0 +1,51 @@
+/*
+ * @Author: AGOGIN 
+ * @Date: 2019-08-02 10:34:20 
+ * @Last Modified by: AGOGIN
+ * @Last Modified time: 2019-08-02 16:38:52
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void Compute_Prefix(char* nums, int numslen, int Next[]) {
+          Next[0] = -1;
+          int k = -1;
+          for (int q = 1; q < numslen; q++) {
+                    while (k >= 0 && nums[q] != nums[k + 1]) {
+                              k = Next[k];
+                    }
+                    if (nums[q] == nums[k + 1]) {
+                              k++;
+                    }
+                    Next[q] = k;
+          }
+}
+
+int KMP(char* T, int numsTlen, char*P, int numsPlen) {
+          int Next[numsPlen];
+          Compute_Prefix(P, numsPlen, Next);
+          int q = -1;
+          int i;
+          for (i = 0; i < numsTlen; i++) {
+                    while (q >=0 && P[q + 1] != T[i]) {
+                              q = Next[q];
+                    }
+                    if (P[q + 1] == T[i]) {
+                              q = q + 1;
+                    }
+                    if (q == numsPlen - 1) {
+                              return i - q;
+                    }
+          }
+          return -1;
+}
+
+main() {
+          char T[] = "cdabababaca";
+          char P[] = "ababaca";
+          int numsTlen = strlen(T);
+          int numsPlen = strlen(P);
+          int ret = KMP(T, numsTlen, P, numsPlen);
+          printf("%d\n", ret);
+}
