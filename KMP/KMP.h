218f24c779f1554c0b1d50171bb3e2bc6a91feb8
NINGNINGGAO <707288044@qq.com>
2019-08-04 15:39:45 +0800

    add .vscode 8/4 15/39

--- KMP/KMP.h
+++ KMP/KMP.h
@@ -2,7 +2,7 @@
  * @Author: AGOGIN 
  * @Date: 2019-08-02 10:34:25 
  * @Last Modified by: AGOGIN
- * @Last Modified time: 2019-08-02 16:52:15
+ * @Last Modified time: 2019-08-02 17:02:57
  */
 #ifndef _KMP_H
 #define _KMP_H
