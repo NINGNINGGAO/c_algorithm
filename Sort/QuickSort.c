5ff3a9a2c956c8cb2a5b73e79ddc0f9dc683e058
NINGNINGGAO <agogin@qq.com>
2019-12-15 22:06:21 +0800

    add quickSort 19/12/15 22/06

--- Sort/QuickSort.c
+++ Sort/QuickSort.c
@@ -1,41 +1,31 @@
 /*
- * @Author: AGOGIN 
- * @Date: 2019-07-28 16:08:51 
- * @Last Modified by:   AGOGIN 
- * @Last Modified time: 2019-07-28 16:08:51 
+ * @Author: AGOGIN
+ * @Date: 2019-07-28 16:08:51
+ * @Last Modified by:   AGOGIN
+ * @Last Modified time: 2019-07-28 16:08:51
  */
 #include <stdio.h>
 
-int partition(int* nums, int p, int r) {
-          int x = nums[r];
-          int i = p - 1;
-          int temp;
-          for (int j = p; j < r; j++) {
-                    if (nums[j] <= x) {
-                              i = i + 1;
-                              temp = nums[i];
-                              nums[i] = nums[j];
-                              nums[j] = temp;
-                    }
-          }
-          nums[r] = nums[i + 1];
-          nums[i + 1] = x;
-          return i + 1;
-}
-
-void Quick(int* nums, int p, int r) {
-          if (p < r) {
-                    int q = partition(nums, p, r);
-                    Quick(nums, p, q - 1);
-                    Quick(nums, q, r);
-          }
+void QuickSort(int* nums, int begin, int end) {
+    if (begin >= end) return;
+    int p = begin, q = end;
+    int temp = nums[p];
+    while (p < q) {
+        while (p < q && nums[q] >= temp) q--;
+        if (p < q) nums[p] = nums[q];
+        while (p < q && nums[p] < temp) p++;
+        if (p < q) nums[q] = nums[p];
+    }
+    nums[p] = temp;
+    QuickSort(nums, begin, p - 1);
+    QuickSort(nums, p + 1, end);
 }
 
 main() {
-          int nums[] = {7,8,9,4,5,6,1,2,3,0,17,18,19,14,15,16,11,12,13,10};
-          int numsSize = sizeof(nums) / sizeof(int);
-          Quick(nums, 1,numsSize - 1);
-          for (int i = 0; i < numsSize; i++) {
-                    printf("%d\n", nums[i]);
-          }
+    int nums[] = {7,8,9,4,5,6,1,2,3,0,17,18,19,14,15,16,11,12,13,10};
+    int numsSize = sizeof(nums) / sizeof(int);
+    QuickSort(nums, 0,numsSize - 1);
+    for (int i = 0; i < numsSize; i++) {
+        printf("%d\n", nums[i]);
+    }
 }
